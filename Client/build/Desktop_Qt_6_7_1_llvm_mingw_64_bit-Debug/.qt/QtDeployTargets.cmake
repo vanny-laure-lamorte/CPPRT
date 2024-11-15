752baabc566585e13e9d6335d5be2b0adf5ce44c
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-11-15 09:41:39 +0100

    <feat>[all] Init architecture

---
+++ Client/build/Desktop_Qt_6_7_1_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Client_FILE C:/Qt/projet/CPPRT/Client/build/Desktop_Qt_6_7_1_llvm_mingw_64_bit-Debug/Client.exe)
+set(__QT_DEPLOY_TARGET_Client_RUNTIME_DLLS C:/Qt/6.7.1/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/llvm-mingw_64/bin/Qt6Network.dll;C:/Qt/6.7.1/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/llvm-mingw_64/bin/Qt6Core.dll)
